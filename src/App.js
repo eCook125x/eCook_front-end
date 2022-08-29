0911b223c7c050c5ef876a82f79268d90e910e2a
bmooo <a0928458481@gmail.com>
2022-08-29 15:53:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react"
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import About from "./pages/About/About";
-import Login from "./pages/Login/Login";
+import Login from "./pages/Login/login";
 import Link from "./pages/Link/Link";
 import Cookbook from "./pages/Cookbook/Cookbook";
 import SweetAndSavory from "./pages/SweetAndSavory/SweetAndSavory";
@@ -17,51 +17,37 @@ import PersonalLRDirections from "./pages/PersonalLR-Directions/PersonalLR-Direc
 import Bookmark from "./pages/Bookmark/Bookmark";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-
-        <Route path='/' element={<Login title="eCook 高齡餐廚巧幫手"/>} />
-        
-      </Routes>
-      <Routes>
-
-        <Route path='/complete' element={<Complete />} />
-
-      </Routes>
-      <Routes>
-
-        <Route path='/link' element={<Link />} />
-        
-      </Routes>
-      <Routes>
-
-        <Route path='/about' element={<About />} />
-        
-      </Routes>
-      <Routes>
-
-        <Route path='/cookbook' element={<Cookbook />} />
-        <Route path='/cookbook/SweetAndSavory' element={<SweetAndSavory />} />
-        <Route path='/cookbook/SweetAndSavory/MealRecipes' element={<MealRecipes />} />
-        
-      </Routes>
-      <Routes>
-
-        <Route path='/LearningRecord' element={<LearningRecord />} />
-        <Route path='/LearningRecord/PersonalLR' element={<PersonalLR />} />
-        <Route path='/LearningRecord/PersonalLR/PersonalLR-Directions' element={< PersonalLRDirections />} />
-        <Route path='/LearningRecord/TeamLR' element={<TeamLR />} />
-        <Route path='/LearningRecord/TeamLR/TeamLR-Directions' element={<TeamLRDirections />} />
-        
-      </Routes>
-      <Routes>
-
-        <Route path='/bookmark' element={< Bookmark />} />
-        
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login title="eCook 高齡餐廚巧幫手" />} />
+            </Routes>
+            <Routes>
+                <Route path="/complete" element={<Complete />} />
+            </Routes>
+            <Routes>
+                <Route path="/link" element={<Link />} />
+            </Routes>
+            <Routes>
+                <Route path="/about" element={<About />} />
+            </Routes>
+            <Routes>
+                <Route path="/cookbook" element={<Cookbook />} />
+                <Route path="/cookbook/SweetAndSavory" element={<SweetAndSavory />} />
+                <Route path="/cookbook/SweetAndSavory/MealRecipes" element={<MealRecipes />} />
+            </Routes>
+            <Routes>
+                <Route path="/LearningRecord" element={<LearningRecord />} />
+                <Route path="/LearningRecord/PersonalLR" element={<PersonalLR />} />
+                <Route path="/LearningRecord/PersonalLR/PersonalLR-Directions" element={<PersonalLRDirections />} />
+                <Route path="/LearningRecord/TeamLR" element={<TeamLR />} />
+                <Route path="/LearningRecord/TeamLR/TeamLR-Directions" element={<TeamLRDirections />} />
+            </Routes>
+            <Routes>
+                <Route path="/bookmark" element={<Bookmark />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
